88b3c59de3a7a4bbf2b78886652bc3d7fa903db6
josuegarcia <josuegarcia0026@gmail.com>
2021-03-31 15:05:34 -0400

    aqui yo inserte los datos en firestore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-import './firebase/config';
+import db from './Firebase/Config';
+
+
+//Para crear tabla para un usuario
+const usuario = {
+    nombre: 'melissa',
+    activo: false,
+    fechaNaci: 0
+}
+
+// insert into usuarios ....
+
+//db.collection('usuarios')
+//.add(usuario)
+//.then( docRef => {
+//    console.log( docRef )
+//})
+//.catch( e => console.log ('error', e ))
